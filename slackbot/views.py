310798ed16e63abe3293046c956f1330f538f439
MinhThieu145 <minhthieu135@gmail.com>
2024-06-25 03:16:15 -0400

    first commit

---
+++ slackbot/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse("Hello, World!")
+
+def slacks(request):
+    # This is the view that will be called when the Slackbot sends a request to the /slack/ endpoint.
+    # print(request.POST)
+    print("Response to Slack url", request.body)
+    return HttpResponse("Hello, Slack!")
